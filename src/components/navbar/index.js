1179f17457dfe4370244b9551d179e79f2d08feb
JHallFS <mr.sl3go.jh@gmail.com>
2021-12-28 19:27:29 +0000

    finished scaffolding

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,7 +1,12 @@
 export default function Navbar() {
 	return (
-		<nav className="">
-			Hi Sedona
+		<nav>
+			<h1>James Hall</h1>
+			<button href="/">Home</button>
+			<button href="/about">About Me</button>
+			<button href="/skills">Skills</button>
+			<button href="/examples">Example Projects</button>
+			<button href="/contact">Contact Me</button>
 		</nav>
 	)
 };
