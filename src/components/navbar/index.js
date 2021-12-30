b7c48126029ed622699ea2074643c1a6306747ed
JHallFS <mr.sl3go.jh@gmail.com>
2021-12-30 07:58:20 +0000

    add redux

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,11 +2,13 @@ export default function Navbar() {
 	return (
 		<nav>
 			<h1>James Hall</h1>
-			<button href="/">Home</button>
-			<button href="/about">About Me</button>
-			<button href="/skills">Skills</button>
-			<button href="/examples">Example Projects</button>
-			<button href="/contact">Contact Me</button>
+			<ul>
+				<li><button href="/">Home</button></li>
+				<li><button href="/about">About Me</button></li>
+				<li><button href="/skills">Skills</button></li>
+				<li><button href="/examples">Example Projects</button></li>
+				<li><button href="/contact">Contact Me</button></li>
+			</ul>
 		</nav>
 	)
 };
