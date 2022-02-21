909209339fe1467b518851a83c2661b772719774
JHallFS <mr.sl3go.jh@gmail.com>
2022-02-20 20:35:50 -0800

    scrapped redux

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,14 +1,14 @@
+import { useState } from 'react';
+
 export default function Navbar() {
+	const [navState, setNavState] = useState({
+		clicked: false
+	});
+
+
 	return (
 		<nav>
 			<h1>James Hall</h1>
-			<ul>
-				<li><button href="/">Home</button></li>
-				<li><button href="/about">About Me</button></li>
-				<li><button href="/skills">Skills</button></li>
-				<li><button href="/examples">Example Projects</button></li>
-				<li><button href="/contact">Contact Me</button></li>
-			</ul>
 		</nav>
 	)
 };
