cc14aeebf218e852b58eea4f4abd7e5f02000b43
JHallFS <mr.sl3go.jh@gmail.com>
2022-04-19 16:36:42 -0700

    added about paragraphs

--- src/components/main-page/index.js
+++ src/components/main-page/index.js
@@ -1,9 +1,20 @@
 export default function Hero() {
 	return (
 		<section>
-			<p>Hi! My name is James and I aspire to make a living programming and tinkering with software and websites.</p>
+			<p>Hello! My name is James and my goal is to make a living programming and tinkering with websites and software for anything imaginable.</p>
 			<h1>About Me</h1>
-			
+			<p>Growing up the son of a civil engineer, when I was old enough to start capturing interest in computers was in the early 2000s.
+			AutoCAD made every engineering firm have to buy(and dispose of) generations of hardware suitible to run that era's 3D imaging software.
+			I had easy access to older boxes with pentium 3! processors and got to work tinkering with the networking capabilities that they had.
+			After many hours of GTA Vice City after school in the back computer in my father's buisness, the majority of my computer use would turn to gaming with little interest in any processes that my be running under the hood.
+			<br />
+			<br />
+			When I graduated high school. I immediately jumped into the job pool and lived my life as a high school graduate with a very large amount of experience in retail(up to management), cash handling, and customer service.
+			Within the last few years I have made a small transition into the auto industry, putting a couple years under my belt as a porter/apprentice body tech before I met my fiance and decided to move up to Olympia Washington.
+			Life is pretty good up here as I currently work as a tech, doing oil changes at Valvoline. 
+			The fiance is enjoying well earned success! Its an inspiring moment for me as I am currently looking to use the full stack web development certificate I recieved from the University of Arizona to get started in a career in web and software development and settle into the next stage of my working life.
+			Thank you for your consideration.
+			</p>
 		</section>
 	);
 }
