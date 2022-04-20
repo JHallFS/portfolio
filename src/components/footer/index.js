32956f0cbda4237df622db418b42c75745e6df7d
JHallFS <mr.sl3go.jh@gmail.com>
2022-04-19 23:40:29 -0700

    41922 eod

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>All content on site made my myself</p>
+            <p>All content on site made my myself, educational modules and projects will be noted.</p>
         </footer>
     );
 }
--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -4,8 +4,9 @@ export default function Hero() {
 			<h2>Computer enthusiast, aspiring programmer and developer.</h2>
 			<h1>About Me</h1>
 			<h1>Experience and Projects</h1>
-			<h1>Contact Form</h1>
-			<p>Hello! My name is James, I live out of rainy Olympia Washington and my goal is to make a living programming and tinkering with websites and software for anything imaginable.</p>
+			<h1>Contact Me</h1>
+			<p>Hello! My name is James, I live out of rainy Olympia Washington and my goal is to make a living programming and tinkering with websites and software for anything imaginable.
+			Currently looking for work that is either remote or local to Pierce County, WA.</p>
 		</section>
 	);
 }
