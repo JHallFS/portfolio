66e176a3445e6cc5c308f03996f6f0a7a551b5ba
JHallFS <mr.sl3go.jh@gmail.com>
2022-04-26 13:39:20 -0700

    my css was dumb, attempting cards and grid format for homepage

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Hero() {
 	return (
 		<section className="hero">
 			<h2>Computer enthusiast, aspiring programmer and developer.</h2>
-			
-			<Link to="/about">About Me</Link>
-			<Link to="/skills">Experience and Projects</Link>
-			<Link to="/contact">Contact Me</Link>
 
+			<div className="heronav">
+				<Link to="/about">About Me</Link>
+				<Link to="/skills">Experience and Projects</Link>
+				<Link to="/contact">Contact Me</Link>
+			</div>
 			<p>Hello! My name is James, I live out of rainy Olympia Washington and my goal is to make a living programming and tinkering with websites and software for anything imaginable.
 			Currently looking for work that is either remote or local to Pierce County, WA.</p>
 		</section>
+		
 	);
-}
+};
+
