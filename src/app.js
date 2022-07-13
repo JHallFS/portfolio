05db75c34f750db21205b7d7f676ed5cc40edf08
JHallFS <mr.sl3go.jh@gmail.com>
2022-07-12 18:10:51 -0700

    spring abandoned for now

--- src/app.js
+++ src/app.js
@@ -5,36 +5,18 @@ import {
 } from 'react-router-dom';
 
 import {
-    useSpring,
-    animated
-} from 'react-spring';
-
-import {
     Homepage,
     About
 } from './pages';
 
 export default function App() {
-    const props = useSpring({
-        from: {
-            opacity: 0,
-            marginTop: -500
-        },
-        to: {
-            opacity: 1,
-            marginTop: 0
-        }
-    });
-
     return (
-        <animated.div style={props}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
             </BrowserRouter>
-        </animated.div>
 
     )
 }
